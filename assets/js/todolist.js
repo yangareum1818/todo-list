89f3dd51ac8050e94efd80028ad5742e9732d5ae
areumYang <yangareum1818@nate.com>
2023-02-16 03:10:13 +0900

    전체 선택/해제 기능 추가 완료. count를 이용해 전체선택 checked를 true / false 되도록 만들었다.

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -7,7 +7,6 @@ const TODOLIST_KEY = "todolist";
 const CHECKED = "checked";
 const SHOW = "show";
 let spaceToDo = [];
-// let isState = false;
 
 // 로컬스토리지 안에 key값을 저장한다. ( stringify : 오브젝트를 문자형으로 변환한다. )
 function saveToDo() {
@@ -28,21 +27,69 @@ const todoCount = function () {
     if (todo.checked === true) {
       count += 1;
     }
-
-    // checked가 true인 갯수가 0개 이상일 때 "allDeleteBtn" 활성화
-    if (count > 0) {
-      allDeleteBtn.disabled = false;
-    }
-    if (count === 0) {
-      allDeleteBtn.disabled = true;
-    }
   });
+
+  // checked가 true인 갯수가 0개 이상일 때 "allDeleteBtn" 활성화
+  if (count > 0) {
+    allDeleteBtn.disabled = false;
+  }
+  if (count === 0) {
+    allDeleteBtn.disabled = true;
+  }
+
+  // 전체 갯수와 선택된 갯수가 같을 때, 'allCheckedBox' checked가 true, 아니면 false
+  if (spaceToDo.length === count) {
+    allCheckedBox.checked = true;
+  } else {
+    allCheckedBox.checked = false;
+  }
 
   totalCount.innerHTML = `${spaceToDo.length}`;
   seleteCount.innerHTML = `${count}`;
 
   totalCount.append();
   seleteCount.append();
+};
+
+// 선택한리스트 모두삭제 버튼 클릭시
+const todoReallyAllDelete = function (e) {
+  console.log(e);
+  const reallydelete = confirm(
+    "정말 선택한 모든 리스트들을 삭제하시겠습니까 ?"
+  );
+
+  if (reallydelete) {
+    console.log("응 진짜 삭제");
+    deleteToDo(e);
+  } else {
+    console.log("아니야 삭제 안할래");
+  }
+};
+
+// 전체선택 버튼 클릭 시
+const todoallChecked = function (e) {
+  let allChkState = e.target.checked;
+
+  let todoli = todoListUI.querySelectorAll(".list");
+  let todoinput = todoListUI.querySelectorAll(".checkbox");
+
+  spaceToDo.filter((todo) => {
+    allChkState === true ? (todo.checked = true) : (todo.checked = false);
+  });
+
+  // 해당 대상 li, input에 조건문으로 style변화와 checkebox true & false
+  for (let i = 0; i < todoli.length; i++) {
+    if (allChkState === true) {
+      todoli[i].classList.add(CHECKED);
+      todoinput[i].checked = true;
+    } else {
+      todoli[i].classList.remove(CHECKED);
+      todoinput[i].checked = false;
+    }
+  }
+
+  saveToDo();
+  todoCount();
 };
 
 // list 갯수가 0일 경우
@@ -61,61 +108,9 @@ const listZero = function () {
   saveToDo();
 };
 
-// 1. 모두선택("allCheckedBox")클릭시 모든 list의 checked가 "true & false"
-// 2-1. ( 'allCheckBox'가 true일 때, 새로고침 시 checked상태 풀림 ) 즉, 새로고침해도 고정되어야함.
-// totalCount === seleteCount같을 때 checked (true) 아니면, (false);
-// 2-2. ( 'allCheckBox'가 true일 때, 새로고침 시 모든 list가 true됌 ) 즉, 새로고침 안한상태에서도 바뀌어야함.
-const todoallChecked = function (e) {
-  console.log("모두 선택버튼 클릭!!");
-  const allChkState = e.target.checked;
-
-  isState = allChkState;
-
-  let todoli = todoListUI.querySelectorAll(".list");
-  let todoinput = todoListUI.querySelectorAll(".checkbox");
-
-  if (isState === true) {
-    // 해당 대상 li, input에 조건문으로 style변화와 checkebox true & false
-    for (let i = 0; i < todoli.length; i++) {
-      todoli[i].classList.add(CHECKED);
-      todoinput[i].checked = true;
-    }
-  } else {
-    for (let i = 0; i < todoli.length; i++) {
-      todoli[i].classList.remove(CHECKED);
-      todoinput[i].checked = false;
-    }
-  }
-
-  spaceToDo.filter((todo) => {
-    // 로컬스토리지 내부 ture & false
-    isState === true ? (todo.checked = true) : (todo.checked = false);
-  });
-
-  console.log(isState);
-  // if (isState === true) {
-  //   for (let i = 0; i < spaceToDo.length; i++) {
-  //     spaceToDo[i].checked = true;
-  //   }
-  // } else {
-  //   for (let i = 0; i < spaceToDo.length; i++) {
-  //     spaceToDo[i].checked = false;
-  //   }
-  // }
-
-  // for (let i = 0; i < spaceToDo.length; i++) {
-  //   allChkState.checked === true
-  //     ? (spaceToDo[i].checked = true)
-  //     : (spaceToDo[i].checked = false);
-  // }
-
-  saveToDo();
-};
-
 // 리스트 삭제 버튼 클릭
 const deleteToDo = function (e) {
   const todoli = e.target.parentElement;
-  console.log(todoli);
 
   const todoOneDelete = spaceToDo.filter((todo) => {
     return todo.id !== parseInt(todoli.id);
@@ -215,6 +210,7 @@ const init = function () {
   todoForm.addEventListener("submit", todoSubmitHandler);
   todoListUI.addEventListener("change", todoChecked);
   allCheckedBox.addEventListener("click", todoallChecked);
+  allDeleteBtn.addEventListener("click", todoReallyAllDelete);
 };
 
 init();
